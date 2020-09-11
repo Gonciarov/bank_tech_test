3490417a74b638accdbffe6870921885759d2589
gonciarov <gonciarov@yandex.ru>
2020-09-11 12:25:37 +0100

    added mocking for tests

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -3,11 +3,9 @@ require 'statement'
 
 describe 'running operations and printing statement' do
   it 'allows to run operations and prints out statement' do
-    account = Account.new
-    account.deposit(1000)
-    account.deposit(2000)
-    account.withdraw(500)
+    account = double()
+    allow(account).to receive(:history).and_return(["#{Date.today.strftime('%d/%m/%Y')}||    || 1000.00 || 1000.00", "#{Date.today.strftime('%d/%m/%Y')}||    || 2000.00 || 3000.00", "#{Date.today.strftime('%d/%m/%Y')}|| -500.00 ||    || 2500.00"])
     statement = Statement.new(account.history)
-    expect(statement.display).to eq("date  || credit || debit || balance\n 09/09/2020|| -500.00 ||    || 2500.00\n09/09/2020||    || 2000.00 || 3000.00\n09/09/2020||    || 1000.00 || 1000.00")
+    expect(statement.display).to eq("date  || credit || debit || balance\n #{Date.today.strftime('%d/%m/%Y')}|| -500.00 ||    || 2500.00\n#{Date.today.strftime('%d/%m/%Y')}||    || 2000.00 || 3000.00\n#{Date.today.strftime('%d/%m/%Y')}||    || 1000.00 || 1000.00")
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,23 +4,18 @@ require 'statement.rb'
 
 describe Statement do
   describe '#statement_header' do
-    it 'shows history' do
-      account = Account.new
-      account.proceed(500)
-      statement = Statement.new(account.history)
-      expect(statement.statement_header.to_s).to include('date  || credit || debit')
+    it 'has a header' do
+    statement = Statement.new('foo')
+    expect(statement.statement_header).to eq('date  || credit || debit || balance')
     end
   end
 
   describe '#display' do
     it 'shows history' do
-      account = Account.new
-      account.proceed(500)
-      account.proceed(-200)
-      statement = Statement.new(account.history)
-      expect(statement.display).to be_a(String)
-      expect(statement.display).to eq("date  || credit || debit || balance\n 09/09/2020|| -200.00 ||    || 300.00\n09/09/2020||    || 500.00 || 500.00")
+        account = double()
+        allow(account).to receive(:history).and_return(["09/09/2020 || 500.00 ||    || 500.00"])
+        statement = Statement.new(account.history)
+        expect(statement.display).to eq("date  || credit || debit || balance\n 09/09/2020 || 500.00 ||    || 500.00")
     end
   end
-
 end
